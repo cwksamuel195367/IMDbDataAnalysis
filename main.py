0604e72227198b48242e8ad2ed885ebdf807cffe
cwksamuel195367 <>
2021-04-28 23:42:19 +0000

    Had fun

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pandas as pd
 
-df=pd.read_csv('https://raw.githubusercontent.com/cwkteacher/Data/master/movie_metadata.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/cwkteacher/Data/master/movie_metadata.csv')
 
 print(df.head(5))
 print(df.columns)
 
 print(df['movie_title'])
+
+cols = ['movie_title', 'facenumber_in_poster']
+print(df[cols])
+
+#
+
+rating = df[df['imdb_score'] > 7.5]
+print(rating[['movie_title', 'imdb_score']])
+
